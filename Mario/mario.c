01805958729d4b842ae8f3867f188c932f079c01
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2021-02-10 21:15:46 -0500

    Problem set

--- Mario/mario.c
+++ Mario/mario.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+//The program we’ll write will be called mario. And let’s allow the user to decide just how tall the pyramid should be by first prompting them for a positive integer between, say, 1 and 8, inclusive.
 
 //Prompt user for positive integer
 int main(void) {
